9bb5276bfc355323c22d6c1d94cb34133dec382d
LI Xiang <xiangfr007@gmail.com>
2024-02-15 23:39:56 +0100

    0001

---
+++ basic_grammar/basic.py
@@ -0,0 +1,19 @@
+# call function print, input parameter "hello python3"
+
+
+"""
+This is comment line1
+This is comment line2
+multi lines comment example
+"""
+
+# declare a variable named x, assign int value 1
+
+# declare a variable named y, assign int value 1.5
+
+# print result of x+y
+
+
+# declare a variable named my_name, assign value your name
+
+# print my_name
---
+++ basic_grammar/basic_answer.py
@@ -0,0 +1,20 @@
+# call function print, input parameter "hello python3"
+print("hello python3")
+
+"""
+This is comment line1
+This is comment line2
+multi lines comment example
+"""
+
+# declare a variable named x, assign int value 1
+x = 1
+# declare a variable named y, assign int value 1.5
+y=1.5
+# print result of x+y
+print(x+y)
+
+# declare a variable named my_name, assign value your name
+my_name = "LI Xiang"
+# print my_name
+print(my_name)
