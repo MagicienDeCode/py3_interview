35995b8295d04b262e0f78b595f168ae6a8fe994
LI Xiang <xiangfr007@gmail.com>
2024-02-16 00:52:52 +0100

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Python3 Study
+
+- [Subscribe 代码魔术师magiciendecode on Youtube](https://www.youtube.com/channel/UCERe5JqcmPtyo3dzX94by1g?sub_confirmation=1)
+- [Please follow me at bilibili](https://space.bilibili.com/520811931)
+- [Please follow me at ixigua](https://www.ixigua.com/home/105169725727/)
+
+## Index
+
+| # | Title | Todo | Answer | Difficulty | Youtube | Bilibili | Ixiagua |
+|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|
+|[Python3 Basic Grammar 0001]()||[Todo File](https://github.com/MagicienDeCode/py3_interview/blob/master/basic_grammar/basic.py)||[Python3 Basic Grammar 0001]()||Answer||[Youtube](https://www.youtube.com/watch?v=Pmlqcl_Ggkc)||[Bilibili](https://www.bilibili.com/video/BV1wN4y1H7hR)||[Xigua](https://www.ixigua.com/i7335974016466813492)|
---
+++ readme_index_builder.py
@@ -0,0 +1,22 @@
+title = "Python3 Basic Grammar 0001"
+title_link = ""
+todo = "Todo File"
+todo_link = "https://github.com/MagicienDeCode/py3_interview/blob/master/basic_grammar/basic.py"
+answer = "Answer"
+answer_link = "https://github.com/MagicienDeCode/py3_interview/blob/master/basic_grammar/basic_answer.py"
+difficulty = "Easy"
+youtube = "https://www.youtube.com/watch?v=Pmlqcl_Ggkc"
+bilibili = "https://www.bilibili.com/video/BV1wN4y1H7hR"
+xiguaid = "7335974016466813492"
+xigua = "https://www.ixigua.com/i" + xiguaid
+
+
+var1 = "|["+title+"]("+title_link+")"+"|"
+var2 = "|["+todo+"]("+todo_link+")"+"|"
+var3 = "|["+title+"]("+title_link+")"+"|"
+var4 = "|"+answer+"|"
+var5 = "|[Youtube]("+youtube+")"+"|"
+var6 = "|[Bilibili]("+bilibili+")"+"|"
+var7 = "|[Xigua]("+xigua+")"+"|"
+
+print(var1+var2+var3+var4+var5+var6+var7)
