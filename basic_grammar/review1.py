b4ab8dc1e80d819c0abb26b5cd7606923f8169ec
LI Xiang <xiangfr007@gmail.com>
2024-02-25 16:27:28 +0100

    r1

---
+++ basic_grammar/review1.py
@@ -0,0 +1,57 @@
+from typing import List
+
+"""
+# basic functions
+
+print("ni hao") # ni hao
+
+# convert str to int
+print(int("9")) # 9
+
+# max of 3 elements
+print(max(7,8,9)) # 9
+
+# convert int to str
+print(str(1)) # 1
+
+# retrieve length of a list, 
+# a set, a dictionary, a string
+print(len("list set dic ...")) # 16
+
+# verify if a string is empty
+print(not "") # True
+
+# process control
+if 3 > 2 :
+	print("3>2") # only print 3>2
+elif 4 > 3 :
+	print("4>3")
+else:
+	print("else")
+
+counter = 10
+while counter > 7:
+	print(counter) # 10 9 8
+	counter -= 1
+
+name = "xiang"
+for i in range(len(name)):
+	print(name[i],ord(name[i])) # x 120 # i 105 # a 97 # n 110 # g 103
+for i in range(4):
+	print(i) # 0 1 2 3
+for i in range(0,4,2):
+	print(i) # 0 2
+for i in range(10,7,-1):
+	print(i) # 10 9 8
+for i in range(10,7,-1):
+	if i == 10:
+		break
+	print(i) # no output
+for i in range(10,7,-1):
+	if i == 10:
+		continue
+	print(i) # 9 8
+"""
+
+# list
+
