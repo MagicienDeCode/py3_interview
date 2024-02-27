4a9d70505adc8f536d110106a0c3ce0422bcfadc
LI Xiang <xiangfr007@gmail.com>
2024-02-27 23:35:30 +0100

    0014

--- basic_grammar/binary_search/35_74.py
+++ basic_grammar/binary_search/35_74.py
@@ -7,6 +7,7 @@ class Solution35:
         while left + 1 < right:
             mid = left + (right - left) // 2
             if nums[mid] == target:
+                # if value equal to target, can return index as insert postion
                 return mid
             elif nums[mid] > target:
                 right = mid
@@ -20,9 +21,12 @@ class Solution35:
 
 class Solution74:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # first, determine in which row, row[0] <= targat <= row[len-1] 
         row_index = self.rowBinarySearch(matrix,target)
+        # if target is not in matrix, return False
         if row_index == -1:
             return False
+        # use binary search template to find an index, if not found, return -1
         target_index = self.binarySearch(matrix[row_index],target)
         return False if target_index == -1 else True
 
@@ -48,14 +52,17 @@ class Solution74:
         right = len(matrix) - 1
         while left + 1 < right:
             mid = left + (right - left) // 2
+            # we compare the first element in each row with target
             if matrix[mid][0] == target:
                 return mid
             elif matrix[mid][0] > target:
                 right = mid
             else:
                 left = mid
+        # after while loop, we have two rows, matrix[left] and matrix[right], if target is not in these two rows, return -1
         if target < matrix[left][0] or target > matrix[right][len(matrix[0])-1]:
             return -1
+        # since matrix are in non-decreasing order, verify target >= matrix[right][0], if not return left.
         if target >= matrix[right][0]:
             return right
         return left
