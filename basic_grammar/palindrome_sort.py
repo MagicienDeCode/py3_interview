9e13d5235ab33edc0c6638a3568214b16b82dd71
MagicienDeCode <xiangfr007@gmail.com>
2024-11-19 02:55:35 +0100

    fix

--- basic_grammar/palindrome_sort.py
+++ basic_grammar/palindrome_sort.py
@@ -1,9 +1,14 @@
 def sort_palindrome(s:str) -> str:
+    half = sorted(s[:len(s)//2])
+    if len(s)%2 == 0: return ''.join(half) + ''.join(half[::-1])
+    return ''.join(half) + s[len(s)//2] +''.join(half[::-1])
+
+    """
     n = len(s)
     count = [0] * 26
     for c in s:
         count[ord(c)-97] += 1
-    res = ['a'] * n
+    res = ['0'] * n
     next_index = 0
     for i in range(26):
         while count[i] > 0:
@@ -13,10 +18,13 @@ def sort_palindrome(s:str) -> str:
             next_index += 1
             count[i] -= 2
     return "".join(res)
+    """
 
 print(sort_palindrome("baab"))
 print(sort_palindrome("babab"))
 print(sort_palindrome("cbaabc"))
+
+print(sort_palindrome("baaab"))
 
 
     
